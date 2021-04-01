fcdb6baf5e6560fbfa7119363f3a51500fc9a2b9
dif-z <77906566+dif-z@users.noreply.github.com>
2021-04-01 10:36:00 +0700

    Update app.js

--- props/js/app.js
+++ props/js/app.js
@@ -2,6 +2,7 @@ const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.nav-menu');
 
 menu.addEventListener('click', function() {
-  menu.classList.toggle('is-active');
-  menuLinks.classList.toggle('active');
-)}
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+})
+
